8ef4efb29b7be715e877dfc9333a8608c81172ca
Dimitri KAHN <dimitri@d-kahn.net>
2013-12-06 13:07:03 +0100

    add  batches module

---
+++ apps/batches/statistics-batch/src/main/java/fr/k2i/adbeback/batch/statistic/BatchApplication.java
@@ -0,0 +1,78 @@
+package fr.k2i.adbeback.batch.statistic;
+
+import org.springframework.batch.core.Job;
+import org.springframework.batch.core.Step;
+import org.springframework.batch.core.StepContribution;
+import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
+import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
+import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
+import org.springframework.batch.core.scope.context.ChunkContext;
+import org.springframework.batch.core.step.tasklet.Tasklet;
+import org.springframework.batch.repeat.RepeatStatus;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.jdbc.TomcatDataSourceConfiguration;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.security.authentication.encoding.PasswordEncoder;
+import org.springframework.security.authentication.encoding.ShaPasswordEncoder;
+
+/**
+ * User: dimitri
+ * Date: 06/12/13
+ * Time: 11:24
+ * Goal:
+ */
+@Configuration
+@EnableAutoConfiguration
+@ComponentScan(basePackages = "fr.k2i.adbeback")
+@EnableBatchProcessing
+@PropertySource(value = {"classpath:application.properties"})
+public class BatchApplication {
+
+    TomcatDataSourceConfiguration dataSourceConfiguration = new TomcatDataSourceConfiguration();
+
+    @Autowired
+    private JobBuilderFactory jobs;
+
+    @Autowired
+    private StepBuilderFactory steps;
+
+    @Bean
+    protected Tasklet tasklet() {
+        return new Tasklet() {
+            @Override
+            public RepeatStatus execute(StepContribution contribution,
+                                        ChunkContext context) {
+                return RepeatStatus.FINISHED;
+            }
+        };
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder(){
+        return new ShaPasswordEncoder();
+    }
+
+    @Bean
+    public Job job() throws Exception {
+        return this.jobs.get("job").start(step1()).build();
+    }
+
+    @Bean
+    protected Step step1() throws Exception {
+        return this.steps.get("step1").tasklet(tasklet()).build();
+    }
+
+    public static void main(String[] args) throws Exception {
+        // System.exit is common for Batch applications since the exit code can be used to
+        // drive a workflow
+        System.exit(SpringApplication.exit(SpringApplication.run(
+                BatchApplication.class, args)));
+    }
+
+
+}
