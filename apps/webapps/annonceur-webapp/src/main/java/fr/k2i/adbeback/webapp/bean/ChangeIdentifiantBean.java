2e754130662fd98c26416bd05b0bf246282b086a
Dimitri KAHN <dimitri@d-kahn.net>
2014-05-13 11:37:04 +0200

    corrections change Identifiant

--- apps/webapps/annonceur-webapp/src/main/java/fr/k2i/adbeback/webapp/bean/ChangeIdentifiantBean.java
+++ apps/webapps/annonceur-webapp/src/main/java/fr/k2i/adbeback/webapp/bean/ChangeIdentifiantBean.java
@@ -2,6 +2,7 @@ package fr.k2i.adbeback.webapp.bean;
 
 import lombok.Data;
 import org.hibernate.validator.constraints.Email;
+import org.hibernate.validator.constraints.NotEmpty;
 
 import java.io.Serializable;
 
@@ -13,6 +14,7 @@ import java.io.Serializable;
  */
 @Data
 public class ChangeIdentifiantBean implements Serializable{
+    @NotEmpty
     @Email
     private String email;
 }
